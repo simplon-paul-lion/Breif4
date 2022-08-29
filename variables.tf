fa2e5f9a7a731d4cb192712408d5dd70bdaf0298
Celia_new <celia.ouedrao@gmail.com>
2022-08-29 13:32:57 +0200

    new3

---
+++ variables.tf
@@ -0,0 +1,63 @@
+variable "resource_group_name" {
+  default = "brief4_g3"
+}
+
+variable "localisation" {
+  default = "francecentral"
+}
+
+variable "vnet_name" {
+  default = "vnet_brief4_g3"
+}
+
+variable "subnet_bastion" {
+  default = "AzureBastionSubnet"
+}
+
+variable "subnet_gateway" {
+  default = "subnet_gateway"
+}
+
+variable "subnet_appbdd" {
+  default = "subnet_jenkins"
+}
+
+variable "ippublique" {
+  default = "IP_publique"
+}
+
+variable "bastion" {
+  default = "bastion_brief4_g3"
+}
+
+variable "DNS"{
+    default = "dns-g3"
+}
+
+variable "VM-nic"{
+    default = "vm-nic"
+}
+
+variable "config_name"{
+    default = "ip_config_nic"
+}
+
+variable "VM_name"{
+    default = "VM_g3"
+}
+
+variable "computerVM_name"{
+    default = "VMg3"
+}
+
+variable "admin"{
+    default = "celia"
+}
+
+variable "OSdisk_name"{
+    default = "OSdisk"
+}
+
+variable "config_name2"{
+    default = "ip_config_bastion"
+}

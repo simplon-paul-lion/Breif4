b62fb06fdbd0beede5c3c6064fcad5c47166db27
Celia_new <celia.ouedrao@gmail.com>
2022-09-01 11:06:10 +0200

    gateway + debut tls

--- main.tf
+++ main.tf
@@ -1,18 +1,23 @@
-# Create a resource group
+# connexion vm : az network bastion ssh -n "bastion_brief4_g3" --resource-group "brief4_g3" --target-resource-id "/subscriptions/a1f74e2d-ec58-4f9a-a112-088e3469febb/resourceGroups/brief4_g3/providers/Microsoft.Compute/virtualMachines/VM_g3" --auth-type "ssh-key" --username "celia" --ssh-key "C:/Users/utilisateur/.ssh/id_rsa"
+
+## Create a resource group
+
 resource "azurerm_resource_group" "main" {
   name     = var.resource_group_name
   location = var.localisation
 }
 
-# Create a virtual network within the resource group
+## Create a virtual network within the resource group
+
 resource "azurerm_virtual_network" "main" {
   name                = var.vnet_name
   resource_group_name = azurerm_resource_group.main.name
-  location = azurerm_resource_group.main.location
+  location = var.localisation
   address_space       = ["10.1.0.0/16"]
 }
 
-# Create the 3 subnets
+## Create the 3 subnets
+
 resource "azurerm_subnet" "subnet1" {
   name                 = var.subnet_bastion
   resource_group_name  = azurerm_resource_group.main.name
@@ -34,21 +39,34 @@ resource "azurerm_subnet" "subnet3" {
   address_prefixes     = ["10.1.3.0/24"]
 }
 
-# Create the public IP
+## Create the public IP for bastion
+
 resource "azurerm_public_ip" "main" {
   name                = var.ippublique
-  location            = azurerm_resource_group.main.location
+  location            = var.localisation
   resource_group_name = azurerm_resource_group.main.name
   allocation_method   = "Static"
   sku                 = "Standard"
   domain_name_label   = var.DNS
 }
 
-# Create bastion
+## Create the public IP for gateway
+
+resource "azurerm_public_ip" "gateway" {
+  name                = var.ippublique2
+  location            = var.localisation
+  resource_group_name = azurerm_resource_group.main.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+  domain_name_label   = var.DNS2
+}
+
+## Create bastion
+
 resource "azurerm_bastion_host" "main" {
   name                = var.bastion
   location            = var.localisation
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.main.name
   tunneling_enabled   = true
   sku                 = "Standard"
 
@@ -59,10 +77,11 @@ resource "azurerm_bastion_host" "main" {
   }
 }
 
-# Create VM
+## Create VM
+
 resource "azurerm_network_interface" "main" {
   name                = var.VM-nic
-  location            = azurerm_resource_group.main.location
+  location            = var.localisation
   resource_group_name = azurerm_resource_group.main.name
 
   ip_configuration {
@@ -74,27 +93,15 @@ resource "azurerm_network_interface" "main" {
 
 resource "azurerm_linux_virtual_machine" "main" {
   name                = var.VM_name
-  computer_name       = var.computerVM_name
   resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  location            = var.localisation
   size                = "Standard_A1_V2"
-  admin_username      = var.admin
   network_interface_ids = [azurerm_network_interface.main.id,]
 
   admin_ssh_key {
     username   = var.admin
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQDRBSlckGDaO56A0EEpSdi4fVE1dGyt14jb82CS2+McddNmqINOo53h4s/Lo6IPfoGpLegHXT1DRw9qb55XgVFDHhV9aTPvZJaKN3RbxlGrHbQZ2NuWRsU+KBxdKq4Qr94zBYgii/nojAmf56nYN0SMwBOGMdc1IbSybqiOFIZfFo18MATYJL4gInGu4BeyMQoN40tgddzYOx+94orM8FH8HuhhlkQCzvHTyz8Cov5jYyhV088H2hRgvn1E7bJHl8YjsT4wubUvf8ilDu6EzhMntQ1C61L65txyfxYp3E2O1ec8/0sLkW5rHpuyncUu7CbIFHkZ3qFhPeE1LLa7lmrmnu7N4BragVmX1oknR05jv/cucedRowwGnrcqKYiBQ7rVa9cWlY2ayCsmbDm7CwnOZoOebYpFO1RnKx/DJ8r/GgUysHaG9A1d3hdzloEwxbG2Osg2t5J8jf8zMNctb4MkbdZj9eIaHEsNyYHEtBpIZye+8Louf5m8EJGHCeKCksM= utilisateur@UTILISA-IUAPJVS"
-  }
-
-  admin_ssh_key {
-    username   = var.admin2
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC0agEe2jMnbMN3OX6bunAdpOoK7idzd5RajsTYcUuF+x1iqqUIXNqDmn6r518hl/NBzUFHshv5k/lasB5pUbHhS2MLpm9t56d5pHnSd3DfAieIggfrpk4RN5yKyKfYQ9DGNjvXgzXy2lSKnXG8fPrkhReJKGpuXe/se5UkpUtGyXQ4BQsAfaArzp0etWZF/KwyZ7ckfOsb7iHCicgoUjqyXbdpP+EooyhSsj6HRhINQ3OuSmtRhrYZLyYXbI/FuN5mtR/+2D4EOhEHFhlFWObJR7TUYRPOW/rCGF/2hNrqwdwWvSmHNg54wRGWmzmrOHZBgyIOAol20+2LUx53eaKObN8lx2chcHVGwEoLjEqbcTv2iQ/C0sTKrqKnvZyEqVftDS/3wMRt5kz4YwJmH7Bk+dZfzdoZX4lqtoYRZKljSmASn836TsH5SdAaNeD62BJ2Efgp3eKxr3ht5vZ9Ktf/aSxpayQ1rJXAmHYeueJI1p3c3v4cnJBpxwSG6cPNvGE= utilisateur@UTILISA-RDIKR2H"
-  }
-
-  admin_ssh_key {
-    username   = var.admin3
-    public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCno1QZ/tXQ+GT4To3zYouq3iMDhRrksttq00iQmQ5tyhQMKCkgQ3xKj5vu9eOyiBbe9pDclhF8T6Bb+qlvo8CoRJrUgthOb+bg8gfYeOipRYpk9IDYq2/Mlf261hXnmNwI5bZrvj/dD5Y+hj16JHma0B22xUp94BJRPj4vNvaWH1HuCFCdvHXga1XjvjggOl45lg+jDBirSl+nvnuFiiGfBfIa+ZFsYgTfapYIrbDi8PWk2shD7QuJvXiGDEd9dDCNcSe8Dslr8+M6CRRmnpPK8wJZiZrvnrPRw3tRuFLxR5V1ip3YMiuvfktw6Qg+RboJeoSw41qblwpH9bur0dt7vd8+/QiUbGp+x5UPn2PpRmOKqTMbRno3RTxaQFaemRzszn0vJIq4bH9NloVELo0GhIcadAmlIdAdLX63NoNJ2LCv6hZhBLJNtf5PZVjkWBYgbsxFPyu6bP532TAg3iwj40sFCXHXzqaTYSm1bzhCk+RGrdvcB760D1aoTXLEKqzoeocjpAuL6lI0JBvKMO+tEpS0M+cHPOTgPz5ZLEbjDdZHw4PzFyt0LaelPwXN8fl3hU89HH5E1iT/qPSCQTIwMnU9y+1fojMuXPYC7lb8bRFcUyAB5L35NOFhIiEZtd50NZsndAelJhXV9zQ9bj55MjYKQVgpl8RdbtpTUE8UgQ== rajac@LAPTOP-FI95FL62"
-  }
+    public_key = file("C:/Users/utilisateur/.ssh/id_rsa.pub")
+    }
 
   os_disk {
     name                 = var.OSdisk_name
@@ -108,11 +115,17 @@ resource "azurerm_linux_virtual_machine" "main" {
     sku       = "16.04-LTS"
     version   = "latest"
   }
+  
+  computer_name       = var.computerVM_name
+  disable_password_authentication = true
+  admin_username = var.admin
 }
+
+## Create NSG
 
 resource "azurerm_network_security_group" "main" {
   name                = var.NSG
-  location            = azurerm_resource_group.main.location
+  location            = var.localisation
   resource_group_name = azurerm_resource_group.main.name
 
   security_rule {
@@ -123,7 +136,7 @@ resource "azurerm_network_security_group" "main" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_ranges    = ["22"]
-    source_address_prefix      = azurerm_public_ip.main.ip_address
+    source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
@@ -135,4 +148,153 @@ resource "azurerm_network_security_group" "main" {
 resource "azurerm_network_interface_security_group_association" "main" {
   network_interface_id      = azurerm_network_interface.main.id
   network_security_group_id = azurerm_network_security_group.main.id
+}
+
+## Create mariadb database
+
+resource "azurerm_mariadb_server" "main" {
+  name = "g3mariadb"
+  location = var.localisation
+  resource_group_name = azurerm_resource_group.main.name
+
+  administrator_login = "adminmariadb"
+  administrator_login_password = "helloPaulCeliaRaja3"
+
+  sku_name = "B_Gen5_2"
+  storage_mb = 5120
+  version = "10.2"
+
+  auto_grow_enabled = true
+  backup_retention_days = 7 
+  geo_redundant_backup_enabled = false 
+  public_network_access_enabled = true
+  ssl_enforcement_enabled = true
+}
+
+resource "azurerm_mariadb_database" "main" {
+  name = "mariadb"
+  resource_group_name = azurerm_resource_group.main.name 
+  server_name = azurerm_mariadb_server.main.name
+  charset = "utf8" 
+  collation = "utf8_general_ci"
+
+}
+
+## Create gateway
+
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.main.name}-beap"
+  http_setting_name              = "${azurerm_virtual_network.main.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.main.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.main.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.main.name}-rdrcfg"
+  frontend_port_name             = "${azurerm_virtual_network.main.name}-fpn"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.main.name}-ficn"
+}
+
+resource "azurerm_application_gateway" "main" {
+  name                = "jenkinsproxy"
+  resource_group_name = azurerm_resource_group.main.name
+  location            = var.localisation
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.subnet2.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 80
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.gateway.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+    priority = 100
+  }
+}
+
+## Create keyvault
+
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "main" {
+  name                        = "keyvault-g3"
+  location                    = var.localisation
+  resource_group_name         = azurerm_resource_group.main.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.object_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_storage_account" "main" {
+  name                     = "g3stockage"
+  resource_group_name      = azurerm_resource_group.main.name
+  location                 = var.localisation
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+  }
+}
+
+resource "azurerm_storage_container" "main" {
+  name                  = "g3conteneur"
+  storage_account_name  = azurerm_storage_account.main.name
+  container_access_type = "private"
 }

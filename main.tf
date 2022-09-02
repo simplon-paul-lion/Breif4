8e2633e103ec6402b3fafc3c3eaafc0d1ecaa00d
simplon-choukriraja <choukri.raja1@gmail.com>
2022-09-02 15:32:52 +0200

    update main

--- main.tf
+++ main.tf
@@ -1,11 +1,13 @@
 
-# Provides the Ressource Group to Logically contain resources 
+# creation resouece group
 
 resource "azurerm_resource_group" "rg" {
   name = var.resource_group_name
   location = var.location
   
 } 
+
+# creation vnet
 
 resource "azurerm_virtual_network" "vnet" {
   name = var.vnet_gr3
@@ -15,6 +17,8 @@ resource "azurerm_virtual_network" "vnet" {
   
 }
 
+# creation subnet vm
+
 resource "azurerm_subnet" "subnet_vm" {
   name = var.subnet_vm
   resource_group_name = azurerm_resource_group.rg.name 
@@ -22,6 +26,8 @@ resource "azurerm_subnet" "subnet_vm" {
   address_prefixes = ["10.1.1.0/24"]
   
 }
+
+# creation subnet bastion
 
 resource "azurerm_subnet" "subnet_bastion" {
   name = var.subnet_bastion
@@ -31,6 +37,8 @@ resource "azurerm_subnet" "subnet_bastion" {
     
 }
 
+# creation subnet gateway
+
 resource "azurerm_subnet" "subnet_gateway" {
   name = var.subnet_gateway
   resource_group_name = azurerm_resource_group.rg.name 
@@ -38,6 +46,37 @@ resource "azurerm_subnet" "subnet_gateway" {
   address_prefixes = ["10.1.2.0/24"]
 
 }
+
+
+
+# creation network interface 
+
+resource "azurerm_public_ip" "adresse_ip" {
+  name                = "adresse_ip"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+}
+
+resource "azurerm_public_ip" "adresse_ipvm" {
+  name                = "adresse_ipvm"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+
+}
+
+resource "azurerm_public_ip" "adresse_ipgw" {
+  name                = "adresse_ipgw"
+  location            = var.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
 
 resource "azurerm_network_interface" "mymv" {
   name = var.interface_gr3
@@ -47,11 +86,29 @@ resource "azurerm_network_interface" "mymv" {
   ip_configuration {
     name = "my_gr3_configuration"
     subnet_id = azurerm_subnet.subnet_vm.id
-    private_ip_address_allocation = "Dynamic"
+    private_ip_address_allocation = "Static"
+    private_ip_address = "10.1.1.10"
+    public_ip_address_id = azurerm_public_ip.adresse_ipvm.id
   }
 }
 
-  
+
+# creation viruel machine linux
+
+data "template_file" "script" {
+  template = "${file("cloud-init.yml")}"
+}
+
+data "template_cloudinit_config" "main" {
+  gzip          = true
+  base64_encode = true
+
+  part {
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.script.rendered}"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "mymv" {
   name = "mv_gr3" 
   location = var.location
@@ -75,24 +132,19 @@ resource "azurerm_linux_virtual_machine" "mymv" {
     }
 
     computer_name  = "myvm"
-    admin_username  = "azureuser"
+    admin_username  = "raja"
     disable_password_authentication = true
+    custom_data = data.template_cloudinit_config.main.rendered
 
     admin_ssh_key {
-    username   = "azureuser"
+    username   = "raja"
     public_key = file ("C:/Users/rajac/.ssh/id_rsa.pub")
   }
 
 }
 
-resource "azurerm_public_ip" "adresse_ip" {
-  name                = "adresse_ip"
-  location            = var.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku                 = "Standard"
+# creation bastion 
 
-}
 resource "azurerm_bastion_host" "vmbastion" {
   name = "vm.bastion"
   location = var.location
@@ -169,12 +221,134 @@ resource "azurerm_mariadb_database" "mariadb" {
 
 }
 
+resource "azurerm_mariadb_firewall_rule" "main" {
+  name                = "firewall-rule"
+  resource_group_name = azurerm_resource_group.rg.name
+  server_name         = azurerm_mariadb_server.servermdb.name
+  start_ip_address    = azurerm_public_ip.adresse_ipvm.ip_address
+  end_ip_address      = azurerm_public_ip.adresse_ipvm.ip_address
+}
 
- 
-  
+
+
+locals {
+  backend_address_pool_name      = "${azurerm_virtual_network.vnet.name}-beap"
+  http_setting_name              = "${azurerm_virtual_network.vnet.name}-be-htst"
+  listener_name                  = "${azurerm_virtual_network.vnet.name}-httplstn"
+  request_routing_rule_name      = "${azurerm_virtual_network.vnet.name}-rqrt"
+  redirect_configuration_name    = "${azurerm_virtual_network.vnet.name}-rdrcfg"
+  frontend_port_name             = "${azurerm_virtual_network.vnet.name}-fpn"
+  frontend_ip_configuration_name = "${azurerm_virtual_network.vnet.name}-ficn"
+}
+
+resource "azurerm_application_gateway" "jenkinsproxy" {
+  name                = "jenkinsproxy"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+
+  sku {
+    name     = "Standard_v2"
+    tier     = "Standard_v2"
+    capacity = 2
+  }
+
+  gateway_ip_configuration {
+    name      = "my-gateway-ip-configuration"
+    subnet_id = azurerm_subnet.subnet_gateway.id
+  }
+
+  frontend_port {
+    name = local.frontend_port_name
+    port = 8080
+  }
+
+  frontend_ip_configuration {
+    name                 = local.frontend_ip_configuration_name
+    public_ip_address_id = azurerm_public_ip.adresse_ipgw.id
+  }
+
+  backend_address_pool {
+    name = local.backend_address_pool_name
+
+  }
+
+  backend_http_settings {
+    name                  = local.http_setting_name
+    cookie_based_affinity = "Disabled"
+    path                  = "/path1/"
+    port                  = 80
+    protocol              = "Http"
+    request_timeout       = 60
+  }
+
+  http_listener {
+    name                           = local.listener_name
+    frontend_ip_configuration_name = local.frontend_ip_configuration_name
+    frontend_port_name             = local.frontend_port_name
+    protocol                       = "Http"
+  }
+
+  request_routing_rule {
+    name                       = local.request_routing_rule_name
+    rule_type                  = "Basic"
+    http_listener_name         = local.listener_name
+    backend_address_pool_name  = local.backend_address_pool_name
+    backend_http_settings_name = local.http_setting_name
+    priority = 100
+  }
+}
 
 
 
+resource "azurerm_network_interface_application_gateway_backend_address_pool_association" "appgwj" {
+  network_interface_id    = azurerm_network_interface.mymv.id
+  ip_configuration_name   = "my_gr3_configuration"
+  backend_address_pool_id = azurerm_application_gateway.jenkinsproxy.backend_address_pool[0].id 
+}
 
+## Create keyvault
 
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_key_vault" "keyvaultgr3" {
+  name                        = "keyvaultg3"
+  location                    = var.location
+  resource_group_name         = azurerm_resource_group.rg.name
+  enabled_for_disk_encryption = true
+  tenant_id                   = data.azurerm_client_config.current.tenant_id
+  soft_delete_retention_days  = 7
+  purge_protection_enabled    = false
+
+  sku_name = "standard"
+
+  access_policy {
+    tenant_id = data.azurerm_client_config.current.tenant_id
+    object_id = data.azurerm_client_config.current.client_id
+
+    key_permissions = [
+      "Get",
+    ]
+
+    secret_permissions = [
+      "Get",
+    ]
+
+    storage_permissions = [
+      "Get",
+    ]
+  }
+}
+
+resource "azurerm_storage_account" "storagegr3" {
+  name                     = "storagegr3"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = var.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
+
+  tags = {
+    environment = "staging"
+
+  }
+}
 

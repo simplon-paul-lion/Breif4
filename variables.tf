c4b4b5e9b1fa16a293b5462e63c30daf9cbc2710
simplon-paul-lion <108001965+simplon-paul-lion@users.noreply.github.com>
2022-09-05 10:06:26 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "group_label" {
+  default = "paul_test"
+}
+
+variable "location" {
+  default = "francecentral"
+}
+
+variable "subid" {
+  default = "a1f74e2d-ec58-4f9a-a112-088e3469febb"
+
+}

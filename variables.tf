c9eafad1599579a8d2d47c8224dbefda74f685ef
Celia_new <celia.ouedrao@gmail.com>
2022-09-05 10:49:22 +0200

    variables commun

--- variables.tf
+++ variables.tf
@@ -18,15 +18,15 @@ variable "subnet_gateway" {
   default = "subnet_gateway"
 }
 
-variable "subnet_appbdd" {
+variable "subnet_vm" {
   default = "subnet_jenkins"
 }
 
-variable "ippublique" {
+variable "ip_bastion" {
   default = "IP_publique_bastion"
 }
 
-variable "ippublique2" {
+variable "ip_vm" {
   default = "IP_publique_vm"
 }
 
@@ -34,11 +34,11 @@ variable "bastion" {
   default = "bastion_brief4_g3"
 }
 
-variable "DNS"{
+variable "DNS_bastion"{
     default = "dns-g3"
 }
 
-variable "DNS2"{
+variable "DNS_vm"{
     default = "dns-vm-g3"
 }
 
@@ -46,7 +46,7 @@ variable "VM-nic"{
     default = "vm-nic"
 }
 
-variable "config_name"{
+variable "config_vm"{
     default = "ip_config_nic"
 }
 
@@ -59,22 +59,14 @@ variable "computerVM_name"{
 }
 
 variable "admin"{
-    default = "celia"
-}
-
-variable "admin2"{
-    default = "paul"
-}
-
-variable "admin3"{
-    default = "raja"
+    default = "admin"
 }
 
 variable "OSdisk_name"{
     default = "OSdisk"
 }
 
-variable "config_name2"{
+variable "config_bastion"{
     default = "ip_config_bastion"
 }
 
@@ -83,5 +75,49 @@ variable "NSG"{
 }
 
 variable "VM_rule"{
-    default = "VM_Bastion_rule"
+    default = "SSH"
+}
+
+variable "VM_rule2"{
+    default = "HTTP"
+}
+
+variable "server_name"{
+    default = "g3server"
+}
+
+variable "mariadb_admin"{
+    default = "adminmariadb"
+}
+
+variable "mariadb_password"{
+    default = "helloPaulCeliaRaja3"
+}
+
+variable "mariadb_name"{
+    default = "g3mariadb"
+}
+
+variable "mariadb_rule"{
+    default = "mariadb_VM_rule"
+}
+
+variable "gateway_name"{
+    default = "jenkinsproxy"
+}
+
+variable "gateway_config"{
+    default = "gateway-ip-configuration"
+}
+
+variable "keyvault_name"{
+    default = "keyvault-g3"
+}
+
+variable "storage_name"{
+    default = "g3stockage"
+}
+
+variable "container_name"{
+    default = "g3conteneur"
 }

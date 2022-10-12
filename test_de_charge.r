b890ec00ca53b48ffdf11319d707df73130e545d
CeliaOuedraogo <108000888+Simplon-CeliaOuedraogo@users.noreply.github.com>
2022-10-12 16:38:33 +0200

    Create test_de_charge.r

---
+++ test_de_charge.r
@@ -0,0 +1,16 @@
+#!usr/bien/env Rscript
+charge_r = read.csv("/home/celia/charge/charge.csv")
+colnames(charge_r) = c("Temps", "Instance")
+library(data.table)
+library(ggplot2)
+library("dplyr")
+charge_rbis=setDT(charge_r)[,list(Count=.N),names(charge_r)]
+charge <- charge_rbis %>%
+  group_by(Instance) %>%
+  dplyr::mutate(Requests = cumsum(Count))
+charge=data.frame(charge)
+print(charge)
+pdf(file="charge.pdf")
+ggplot(data=charge, aes(x=Temps, y=Requests, group = Instance, color = Instance))+geom_line()
+pdf(file="charge_bis.pdf")
+ggplot(data=charge, aes(x=Temps, y=Count, group = Instance, color = Instance))+geom_line()

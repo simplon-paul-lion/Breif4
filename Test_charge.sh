70740f7aa9f9bd259c6db20c8c5cc6594a2ce89a
CeliaOuedraogo <108000888+Simplon-CeliaOuedraogo@users.noreply.github.com>
2022-10-13 14:16:23 +0200

    Update Test_charge.sh

--- Test_charge.sh
+++ Test_charge.sh
@@ -1,4 +1,4 @@
-seq 500 | parallel --max-args 0  --jobs 20 "  curl -k -iF 'vote=Cats' https://vote.simplon-celia.space" > test_file
+seq 500 | parallel --max-args 0  --jobs 20 "curl -k -iF 'vote=Dogs' https://vote.simplon-celia.space" > test_file
 sed '/ 504 Gateway Time-out/I,+2 d' test_file > test_charge
 grep "^Date" test_charge > date
 grep "^X-HAN" test_charge > handle
@@ -9,5 +9,5 @@ awk '{ if(NR == 1) { shift = $1 } print ($1 - shift) " " $2 }' date2 > date3
 sed -e "s/X-HANDLED-BY://g" -i handle
 paste -d, date3 handle > total
 sed -e "s/ //g" -i total
-
+mv total total.csv
 Rscript test_de_charge.r

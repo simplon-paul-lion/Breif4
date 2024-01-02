82725b46fc4e6f8403fc9bff5205084c1ab1d4cd
Raja <108053084+simplon-choukriraja@users.noreply.github.com>
2024-01-02 20:11:43 +0100

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "resource_group_name" {
+  default = "projet-raja"
+} 
+
+variable "location" {
+  default = "westeurope"
+}
+
+variable "vnet_j" {
+  default = "vnet_j"
+}
+
+variable "subnet_j" {
+  default = "subnet_j"
+}
+
+
+variable "ip_j" {
+  default = "ip_j"
+}
